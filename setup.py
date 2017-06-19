dcd9aa88cc0c2a0bad688b8b6af165c1c60b7b0d
Kieran Browne <kierantbrowne@gmail.com>
2017-06-19 13:49:22 +1000

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='why',
+        version='0.0.1',
+        description='TODO',
+        url='http://github.com/kieranbrowne/why',
+        author='Kieran Browne',
+        author_email='kieranbrowne@gmail.com',
+        license='MIT',
+        packages=['why'],
+        zip_safe=False)
